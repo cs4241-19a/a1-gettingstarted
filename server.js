4388ce6b931ca57741f1e0f59585bc78cbc12675
manasmehta18 <manasmehta.18@gmail.com>
2019-08-22 21:42:56 -0400

    added styelsheet and edited server.js to include it

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ fs.readFile( './index.html', function( err, content ) {
   file = content
 })
 
+let stylesheet
+fs.readFile( './style.css', function( err, content ) {
+  stylesheet = content
+})
+
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
       response.end( file, 'utf-8' )
+      break
+    case '/index.html':
+      response.end( file, 'utf-8' )
+      break
+    case '/style.css':
+      response.end( stylesheet, 'utf-8' )
       break
     default:
       response.end( '404 Error: File Not Found' )
