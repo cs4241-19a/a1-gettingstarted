e7d983fb4e40b30ea4f0373ea519b4d32939d41c
James Plante <jplante@wpi.edu>
2019-08-24 12:36:58 -0400

    Fixed the server to serve up the image and CSS files

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+	fs   = require('fs'),
+	port = 3000
 
-let file
+let file, picture, style
+
+/* Read all files before loading page */
+console.log("Loading files into memory...")
 fs.readFile( './index.html', function( err, content ) {
-  file = content
+	file = content;
 })
 
+fs.readFile('./P7.jpg', function(err, content) {
+	picture = content;
+})
+
+fs.readFile('./style.css', function(err, content){
+	style = content;
+})
+console.log("Starting Server!")
+
+/* This callback is made every time a request is given. */
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      response.end( file, 'utf-8' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+	console.log("New request! The user is asking for " + request.url)
+	switch( request.url ) {
+		case '/':
+			response.end( file, 'utf-8' )
+			break;
+		case '/P7.jpg':
+			response.end(picture)
+			break;
+		case './style.css':
+			response.end( style, 'utf-8')
+		default:
+			response.end( '404 Error: File Not Found' )
+	}
 })
 
 server.listen( process.env.PORT || port )
