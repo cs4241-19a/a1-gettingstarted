400070fad30da2f9221a6d57fd05e077c6575828
Karen Royer <kmroyer@wpi.edu>
2019-08-24 15:53:32 -0400

    testing css font styles

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ fs.readFile( './index.html', function( err, content ) {
   file = content
 })
 
+let cssFile
+fs.readFile( './a1_gettingstarted_style.css', function( err, content ) {
+  cssFile = content
+})
+
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
       response.end( file, 'utf-8' )
+      break
+      case '/index.html':
+      response.end( file, 'utf-8')
+      break
+      case '/a1_gettingstarted_style.css':
+      response.end( cssFile, 'utf-8')
       break
     default:
       response.end( '404 Error: File Not Found' )
