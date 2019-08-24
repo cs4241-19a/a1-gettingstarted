20d39792215779449cc8f2cae58680a6da0d5702
Glitch (a1-jsthompson16) <jsthompson@wpi.edu>
2019-08-24 18:42:13 +0000

    Give full class names, add line break
    ./server.js:6266538/364
    ./index.html:6266538/221

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ fs.readFile( './index.html', function( err, content ) {
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8' )
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
       break
     default:
       response.end( '404 Error: File Not Found' )
@@ -19,3 +22,9 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
