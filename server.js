3d2dcdc4067961add8eadc32dd8de03b70579ce9
manasmehta18 <manasmehta.18@gmail.com>
2019-08-24 12:13:33 -0400

    added bg and typewriter for front page

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ fs.readFile( './style.css', function( err, content ) {
   stylesheet = content
 })
 
+let bg
+fs.readFile( './bg/2.jpg', function( err, content ) {
+  bg = content
+})
+
+let bg1
+fs.readFile( './bg/4.jpg', function( err, content ) {
+  bg1 = content
+})
+
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
@@ -22,6 +32,12 @@ const server = http.createServer( function( request,response ) {
       break
     case '/style.css':
       response.end( stylesheet, 'utf-8' )
+      break
+    case '/bg/2.jpg':
+      response.end( bg, 'utf-8' )
+      break
+    case '/bg/4.jpg':
+      response.end( bg1, 'utf-8' )
       break
     default:
       response.end( '404 Error: File Not Found' )
