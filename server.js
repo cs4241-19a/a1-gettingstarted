84ba5c7851e254f57b6b92f40a1f08cd6281814c
manasmehta18 <manasmehta.18@gmail.com>
2019-08-24 16:22:16 -0400

    added jquery, navbar, scroll and edited server for new function

--- server.js
+++ server.js
@@ -2,42 +2,31 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-let file
-fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
-
-let stylesheet
-fs.readFile( './style.css', function( err, content ) {
-  stylesheet = content
-})
-
-let bg
-fs.readFile( './bg/2.jpg', function( err, content ) {
-  bg = content
-})
-
-let bg1
-fs.readFile( './bg/4.jpg', function( err, content ) {
-  bg1 = content
-})
-
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8' )
+      sendFile( response, 'index.html' )
       break
     case '/index.html':
-      response.end( file, 'utf-8' )
+      sendFile( response, 'index.html' )
       break
     case '/style.css':
-      response.end( stylesheet, 'utf-8' )
+      sendFile( response, 'style.css' )
       break
     case '/bg/2.jpg':
-      response.end( bg, 'utf-8' )
+      sendFile( response, 'bg/2.jpg' )
       break
     case '/bg/4.jpg':
-      response.end( bg1, 'utf-8' )
+      sendFile( response, 'bg/4.jpg' )
+      break
+    case '/Scripts/NavBar.js':
+      sendFile( response, 'Scripts/NavBar.js' )
+      break
+    case '/Scripts/scroll.js':
+      sendFile( response, 'Scripts/scroll.js' )
+      break
+    case '/node_modules/jquery/src/jquery.js':
+      sendFile( response, 'node_modules/jquery/src/jquery.js' )
       break
     default:
       response.end( '404 Error: File Not Found' )
@@ -45,4 +34,11 @@ const server = http.createServer( function( request,response ) {
 })
 
 server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+  fs.readFile( filename, function( err, content ) {
+    file = content
+    response.end( content, 'utf-8' )
+  })
+}
 
