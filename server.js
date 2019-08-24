f2c3a2607db23fb5e054ff50bb9425e967749bd7
Glitch (a1-jrbartone) <none>
2019-08-24 22:34:30 +0000

    I started my homework

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-let file
-fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
-
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8')
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
       break
     default:
-      response.end( '404 Error: File Not Found')
+      response.end( '404 Error: File Not Found' )
   }
 })
 
 server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
