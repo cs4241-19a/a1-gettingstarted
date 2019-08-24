145d36d2ac65cbc9b85075ef5c5bf1461ad8f251
Glitch (a1-jsthompson16) <jsthompson@wpi.edu>
2019-08-24 19:15:12 +0000

    No longer necessary
    ./style.css:6266538/12
    ./server.js:6266538/86

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-let file
-fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
-
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
