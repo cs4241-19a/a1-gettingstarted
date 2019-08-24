ab6887d0e55320a7adbf8d4e59e31fb762d09b5f
James Plante <jplante@wpi.edu>
2019-08-24 15:01:14 -0400

    fixed the server in order to incorporate changes from Slack

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ let file, picture, style
 
 /* Read all files before loading page */
 console.log("Loading files into memory...")
-fs.readFile( './index.html', function( err, content ) {
-	file = content;
-})
 
 fs.readFile('./P7.jpg', function(err, content) {
 	picture = content;
 })
 
-fs.readFile('./style.css', function(err, content){
-	style = content;
-})
 console.log("Starting Server!")
 
 /* This callback is made every time a request is given. */
@@ -24,13 +18,14 @@ const server = http.createServer( function( request,response ) {
 	console.log("New request! The user is asking for " + request.url)
 	switch( request.url ) {
 		case '/':
-			response.end( file, 'utf-8' )
+			sendFile(file, response, './index.html', 'utf-8')
 			break;
 		case '/P7.jpg':
-			response.end(picture)
+			sendFile(picture, response, './P7.jpg')
 			break;
-		case './style.css':
-			response.end( style, 'utf-8')
+		case '/style.css':
+			sendFile(style, response, './style.css', 'utf-8')
+			break;
 		default:
 			response.end( '404 Error: File Not Found' )
 	}
@@ -38,3 +33,20 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
+/*
+ * Function to send a file to the client
+ * @param pointer - where to store the file
+ * @param response - final response to the client
+ * @param filename - filename for file
+ * @param format - how to interpret the data in the request
+ */
+const sendFile = function( pointer, response, filename, format=null ) {
+   fs.readFile( filename, function( err, content ) {
+     pointer = content
+	   if (format === null) {
+		   response.end( pointer, format )
+	   } else {
+		   response.end( pointer )
+	   }
+   })
+}
