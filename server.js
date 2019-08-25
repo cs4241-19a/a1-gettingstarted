e220466454027ba2f709440547511fd5f2e2e1e2
Glitch (a1-nbloniarz) <none>
2019-08-25 18:28:07 +0000

    🚧🏝 Checkpoint ./server.js:6266250/335 ./styles.css:6266250/1528 ./index.html:6266250/54

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/styles.css':
+      sendFile( response, 'styles.css')
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -19,7 +22,8 @@ server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
    fs.readFile( filename, function( err, content ) {
-     file = content
+     //Can Add var in front of file to dismiss undefined issue
+     var file = content
      response.end( content, 'utf-8' )
    })
 }
