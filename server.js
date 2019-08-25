f4b3087bbcf8f426bdd71cb29eea4f5c9da54af4
Glitch (yangjack1998-a1-gettingstarted) <none>
2019-08-25 15:36:25 +0000

    💇💾 Checkpoint ./index.html:6271330/60 ./public/style.css:6271330/60 ./server.js:6271330/137

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/mystyle.css':
+      sendFile( response, 'mystyle.css' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
