f9ff814dd72835bab1b47feaeddc77b6040ffca0
ABon27 <drewbon.alt@gmail.com>
2019-08-25 16:58:20 -0400

    initial commit and updated js file

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
-
-let file
-fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
+    fs   = require('fs'),
+    port = 3000
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8')
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
       break
     default:
-      response.end( '404 Error: File Not Found')
+      response.end( '404 Error: File Not Found' )
   }
 })
 
 server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+  fs.readFile( filename, function( err, content ) {
+    file = content
+    response.end( content, 'utf-8' )
+  })
+}
