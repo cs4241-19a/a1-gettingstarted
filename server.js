c9e789012437a011ef76a3f15ad7889e74955de4
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-08-26 13:27:52 -0400

    new stuff

--- server.js
+++ server.js
@@ -1,80 +1,39 @@
 const http = require('http'),
       fs   = require('fs'),
+      mime = require('mime'),
       port = 3000
-
-let file
-fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
-let style
-//test
-fs.readFile( './style.css', function( err, content ) {
-  style = content
-})
-let ken
-fs.readFile( './ken.jpeg', function( err, content ) {
-  ken = content
-})
-let html
-fs.readFile( './html.png', function( err, content ) {
-  html = content
-})
-let css
-fs.readFile( './css.png', function( err, content ) {
-  css = content
-})
-let js
-fs.readFile( './js.png', function( err, content ) {
-  js = content
-})
-let java
-fs.readFile( './java.png', function( err, content ) {
-  java = content
-})
-let ruby
-fs.readFile( './ruby.png', function( err, content ) {
-  ruby = content
-})
-let python
-fs.readFile( './python.png', function( err, content ) {
-  python = content
-})
-let test
-fs.readFile( './test.png', function( err, content ) {
-  test = content
-})
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8')
+      sendFile( response, 'index.html' )
       break
     case '/ken.jpeg':
-      response.end( ken, 'utf-8')
+      sendFile( response, 'ken.jpeg' )
       break
     case '/html.png':
-      response.end( html, 'utf-8')
+      sendFile( response, 'html.png' )
       break
     case '/css.png':
-      response.end( css, 'utf-8')
+      sendFile( response, 'css.png' )
       break
     case '/js.png':
-      response.end( js, 'utf-8')
+      sendFile( response, 'js.png' )
       break
     case '/java.png':
-      response.end( java, 'utf-8')
+      sendFile( response, 'java.png' )
       break
     case '/ruby.png':
-      response.end( ruby, 'utf-8')
+      sendFile( response, 'ruby.png' )
       break
     case '/python.png':
-      response.end( python, 'utf-8')
+      sendFile( response, 'python.png' )
       break
     case '/test.png':
-      response.end( test, 'utf-8')
+      sendFile( response, 'test.png' )
       break
     case '/style.css':
-      response.end( style, 'utf-8')
+      sendFile( response, 'style.css' )
       break
     default:
       response.end( '404 Error: File Not Found')
@@ -82,3 +41,12 @@ const server = http.createServer( function( request,response ) {
 })
 
 server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+  const mimeType = mime.getType(filename)
+  response.writeHeader(200, {'Content-Type': mimeType})
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
