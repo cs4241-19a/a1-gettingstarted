944b31d8a4cd1dc13bc2f8cb9dde53924ee5b810
mjadiletta <mjadiletta@wpi.edu>
2019-08-26 21:39:04 -0400

    assignment 1 final updates except readme

--- server.js
+++ server.js
@@ -5,16 +5,38 @@ const http = require('http'),
 let file
 fs.readFile( './index.html', function( err, content ) {
   file = content
-})
+}) 
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+let cssFile
+fs.readFile('./bulma.min.css', function(err, content) {
+	cssFile = content
+}) 
+
+
+const server = http.createServer( function( request, response ) {
+  switch( request.url ) { 
+    case '/bulma.min.css':
+      response.writeHead(200, {"Content-Type": "text/css"});
+      response.write(cssFile);
     case '/':
-      response.end( file, 'utf-8')
+      sendFile(response, 'index.html')
+      break
+	 case 'index.html':
+      sendFile(response, 'index.html')
       break
     default:
       response.end( '404 Error: File Not Found')
   }
-})
+}) 
 
 server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
+
+
+
