46d901a1d37396c9da1f9d4b668ca52823481d29
Carla Duarte <ciduarte@wpi.edu>
2019-08-26 22:12:37 -0400

    https://a1-ciduarte.glitch.me

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+    fs   = require('fs'),
+    port = 3000
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
@@ -10,6 +10,21 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/images/edx-logo.png':
+      sendFile( response, 'images/edx-logo.png' )
+      break
+    case '/images/hanover-logo.png':
+      sendFile( response, 'images/hanover-logo.png' )
+      break
+    case '/images/nust-logo.png':
+      sendFile( response, 'images/nust-logo.png' )
+      break
+    case '/images/sensitech-logo.png':
+      sendFile( response, 'images/sensitech-logo.png' )
+      break
+    case '/images/wpi-logo.png':
+      sendFile( response, 'images/wpi-logo.png' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -18,8 +33,8 @@ const server = http.createServer( function( request,response ) {
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+  fs.readFile( filename, function( err, content ) {
+    file = content
+    response.end( content, 'utf-8' )
+  })
 }
