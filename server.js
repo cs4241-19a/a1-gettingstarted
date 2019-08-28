d9f5ed6c335015ec5915df1f1cce4998954ae119
John Zarate <git@jcharante.com>
2019-08-28 15:26:21 -0400

    update server

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const http = require('http'),
       fs   = require('fs'),
+      mime = require('mime'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+  if (request.url === '/') {
+    sendFile(response, 'index.html')
+  } else {
+    sendFile(response, request.url.slice(1))
   }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
+  const mimeType = mime.getType(filename)
+  response.writeHeader(200, {'Content-Type': mimeType})
    fs.readFile( filename, function( err, content ) {
-     file = content
      response.end( content, 'utf-8' )
    })
 }
