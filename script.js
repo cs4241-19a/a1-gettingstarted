3dc77d00b3193ef457ca95326f6ba86a8805dbd7
Glitch (a1-nbloniarz) <none>
2019-08-28 19:56:49 +0000

    🐇🏀 Checkpoint ./index.html:6266250/27 ./script.js:6266250/825 ./package.json:6266250/560

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function ChangeCursor(currentState){
-  if(currentState === 1){
-    document.getElementById('')
+function ChangeCursor(){
+  console.log(document.getElementById("Flip"))
+  /*if(document.getElementById('FlipCursor').style.cursor === 1){
+    document.getElementById('FlipCursor').style.cursor = 'https://cdn.glitch.com/da293af5-e591-4bcc-939a-00fb7e595dcb%2Fcropped.gif?v=1566955020998';
   }
   else{
-    
-  }
+    document.getElementById('FlipCursor').style.cursor = 'https://cdn.glitch.com/da293af5-e591-4bcc-939a-00fb7e595dcb%2Flooped.gif?v=1567017618454';
+  }*/
   
 }
   
