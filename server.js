a79a16e75af55c07d437977d08841125a93f51d6
Glitch (vanand23-a1-gettingstarted) <none>
2019-08-28 02:37:43 +0000

    🍽💏 Checkpoint ./server.js:1008515/751 ./style.css:1008515/8

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http'),
+/*const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
@@ -25,4 +25,36 @@ const sendFile = function( response, filename ) {
      filename = content
      response.end( content, 'utf-8' )
    })
+}*/
+
+const http = require( 'http' ),
+      fs = require( 'fs' ),
+      mime = require('mime'),
+      port = 3000
+
+const server = http.createServer(function(request, response){
+  const url = request.url.slice( 1 )
+  if( request.url === '/'){
+    sendFile( response, 'index.html')
+  }
+  else{
+    sendFile(response, url)
+  }
+  
+})
+
+server.listen( process.env.PORT || port ) 
+
+const sendFile = function( response, filename ){
+  const mimeType = mime.getType( filename )
+  response.writeHeader( 200, { "Content-Type" : mimeType})
+  fs.readFile( filename, function( error, content ) {
+    if( error === null){
+      response.end( content, 'utf-8')
+    }else{
+      response.writeHeader(404)
+      response.end("Error 404 file now found")
+    }
+    
+  })
 }
