f147150fe2d6d86399c7b3fd742d9b17a2fdf5c0
Glitch (amandaeze97-a1-gettingstarted) <none>
2019-08-28 16:41:16 +0000

    🏬🚓 Checkpoint ./server.js:2478586/1449 ./package.json:2478586/46 ./index.html:2478586/194

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 const http = require('http'),
       fs   = require('fs'),
+      mime = require( 'mime' ),
       port = 3000
 
-let cssFile;
-fs.readFile( './styles.css', function( err, content ) {
-    cssFile = content;
-});
-
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
+  const filename = request.url.slice( 1 ) 
+
+  console.log( filename )
+
+  switch( filename ) {
+    case '':
+      if( request.url === '/' ) sendFile( response, 'index.html' )
       break
-    case '/styles.css':
-      sendFile( response, 'styles.css' )
-      response.write( cssFile, 'utf-8' );
-      break;
     default:
-      response.end( '404 Error: File Not Found' )
+      sendFile( response, filename )
+      break
   }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     filename = content
-     response.end( content, 'utf-8' )
-   })
+  const type = mime.getType( filename ) 
+  fs.readFile( filename, function( err, content ) {
+    // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
+  })
 }
