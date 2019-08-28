193f3ee4e2af4741f003cec9bbb352b469e876f9
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-08-28 09:04:04 -0400

    new server

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-const http = require('http'),
-      fs   = require('fs'),
-      mime = require('mime'),
+const http = require( 'http' ),
+      fs   = require( 'fs' ),
+      // IMPORTANT: you must run `npm install` in the directory for this assignment
+      // to install the mime library used in the following line of code
+      mime = require( 'mime' ),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  const url = request.url.slice(1)
-  if(request.url === '/'){
-    sendFile(response, url)
-  } else{
-    sendFile(response, url)
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+  const filename = request.url.slice( 1 ) // remove leading forward slash (**not a backslash**)
+
+  console.log( filename )
+  
+  switch( filename ) {
+    case '':
+      if( request.url === '/' ) sendFile( response, 'index.html' )
+      break
+    default:
+      sendFile( response, filename )
+      break
   }
 })
 
 server.listen( process.env.PORT || port )
-
 const sendFile = function( response, filename ) {
-  const mimeType = mime.getType(filename)
-  response.writeHeader(200, {'Content-Type': mimeType})
-   fs.readFile( filename, function( error, content ) {
-    if (error === null){
-      response.end( content, 'utf-8' )
-    } else{
-      response.writeHeader(404)
-      response.end('File not found', 'utf-8')
-    }
+   // mime types: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
    })
 }
-
-/*RENAME GIT REPO*/
