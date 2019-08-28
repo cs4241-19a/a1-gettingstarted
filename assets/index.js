6bee0b7b0ca288fc9c14d677caf607428a62a5de
Glitch (a1-samgoldman) <none>
2019-08-28 23:13:08 +0000

    📼📕 Checkpoint ./index.html:6303385/2745 ./assets/index.js:6303385/1371 ./style.css:6303385/36

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
 function animate(options) {
 
-	var start = performance.now();
+	const start = performance.now();
 
 	requestAnimationFrame(function animate(time) {
-		var timeFraction = (time - start) / options.duration;
+		let timeFraction = (time - start) / options.duration;
 		if (timeFraction > 1) timeFraction = 1;
 
-		var progress = options.timing(timeFraction);
+		const progress = options.timing(timeFraction);
 
 		options.draw(progress);
 
@@ -24,63 +24,54 @@ function makeEaseOut(timing) {
 }
 
 function bounce(timeFraction) {
-	for (var a = 0, b = 1; 1; a += b, b /= 2) {
+	for (let a = 0, b = 1; 1; a += b, b /= 2) {
 		if (timeFraction >= (7 - 4 * a) / 11) {
 			return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
 		}
 	}
 }
 
-function a_dot1() {
-	var to = window.innerHeight/2 - 137;
+function a_dot() {
+	const to1 = window.innerHeight/2 - 137;
 	animate({
 		duration: 3000,
 		timing: makeEaseOut(bounce),
 		draw: function(progress) {
-			dot1.style.top = to * progress + 'px';
+			dot1.style.top = to1 * progress + 'px';
 		}
 	});
-}
-
-function a_dot2() {
-	var to = window.innerHeight/2 - (137 - 89);
+  
+	const to2 = window.innerHeight/2 - (137 - 89);
 	animate({
 		duration: 3000,
 		timing: makeEaseOut(bounce),
 		draw: function(progress) {
-			dot2.style.top = to * progress + 'px';
+			dot2.style.top = to2 * progress + 'px';
 		}
 	});
-}
-
-function a_dot3() {
-	var to = window.innerHeight/2 - (137 - 181);
+  
+	const to3 = window.innerHeight/2 - (137 - 181);
 	animate({
 		duration: 3000,
 		timing: makeEaseOut(bounce),
 		draw: function(progress) {
-			dot3.style.top = to * progress + 'px';
+			dot3.style.top = to3 * progress + 'px';
 		}
 	});
-}
-
-function a_dot4() {
-	var to = window.innerHeight/2 - (137 - 240);
+  
+	const to4 = window.innerHeight/2 - (137 - 240);
 	animate({
 		duration: 3000,
 		timing: makeEaseOut(bounce),
 		draw: function(progress) {
-			dot4.style.top = to * progress + 'px';
+			dot4.style.top = to4* progress + 'px';
 		}
 	});
 }
 
 window.onkeyup = function(e) {
 	var key = e.key;
-	if (key === ' ') {
-		a_dot1();
-		a_dot2();
-		a_dot3();
-		a_dot4();
+	if (key === 'a') {
+		a_dot();
 	}
 };
