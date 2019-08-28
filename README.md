d64fbab692157f9dd78af6339a605c1a8b375de5
kitzeller <kit.zeller@gmail.com>
2019-08-28 19:36:46 -0400

    Final updates

--- README.md
+++ README.md
@@ -7,19 +7,20 @@ https://a1-kitzeller.glitch.me/
 This project shows some of my current HTML, CSS, Javascript and Design skills.
 
 ## Technical Achievements
-- Used knowledge of Babylon.js to implement a custom 3D "grid heightmap" header background
-- Used jQuery.js and TypeIt.js to create a custom interactive experience in which the user
-watches the site "build itself". I figured out how to combine jquery and typeit to create this experience 
-without any additional resources.
+- I used Babylon.js to implement a custom 3D "grid heightmap" header background.
+- I used jQuery.js and TypeIt.js to create a custom interactive experience in which the user
+watches the site "build itself". I had to figure out how to combine jquery and typeit to create this experience 
+without any additional resources, which includes chaining forced scrolling, animations, time delays, and DOM manipulation etc.
+- I added audio play/pause functionality.
 
 ### Design Achievements
-- No templates/external css files were used for this project (besides the default bootstrap css).
-- Designed the interactive experience from scratch, for example, figured out the best timing, 
+- No templates/external css files were used for this project (besides the default bootstrap css which I used to improve the site design).
+- I designed the interactive experience from scratch, for example, I figured out the best timing, 
 color scheme, animation speed, auto-scroll behavior.
-- Implemented a minimalist greyscale design.
-- Attention to minor details to improve interactivity, such as color changes onhover.
+- I implemented a minimalist greyscale design.
+- I paid attention to minor details to improve interactivity, such as color changes on hover.
 - The website is responsive and adapts to mobile version (through use of bootstrap).
-- All the CSS is custom coded for this site.
+- All the CSS is custom coded for this site, including a custom font from [dafont](https://www.dafont.com/).
 - Added soft synthetic sound to further the user experience.
 
 
@@ -30,5 +31,5 @@ color scheme, animation speed, auto-scroll behavior.
 
 **Video Demo (Click Thumbnail)**
 
-[![video](https://img.youtube.com/vi/WkgU7bAWqhg/0.jpg)](https://www.youtube.com/watch?v=WkgU7bAWqhg)
+[![video](https://img.youtube.com/vi/cku6YLOOFbU/0.jpg)](https://www.youtube.com/watch?v=cku6YLOOFbU)
 
--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-// TODO: Clean code
-// TODO: Non-interactive mode
-// TODO: Improve skill bars
-// TODO: Disable scrolling during animations?
-// TODO: Fix width changes?
+
+// Time interval
+var interval;
 
 window.onload = function () {
 
-    // Type It
+    /**
+     * Audio Control
+     */
+    $('#sound').click(function () {
+        if ($("#background-music")[0].paused) {
+            $("#background-music")[0].play();
+        } else {
+            $("#background-music")[0].pause();
+        }
+        $("#sound").toggleClass("fa-pause-circle")
+    });
+
+    /**
+     * TypeIt Functions
+     */
     let startTypeIt = new TypeIt('#start', {
-        speed: 20,
+        speed: 35,
         breakLines: false,
         strings: [
             "Welcome to my website!",
             "Please note that this is an experience and will take just less than a minute.",
+            "You will not be able to scroll around until it is complete.",
+            "You can click the play button in the top left to toggle music.",
+            "Now to begin.",
             "This page looks a little empty...",
             "Let's fix that.",
             "I think we should add a cool background.",
@@ -55,7 +70,7 @@ window.onload = function () {
     }
 
     function stepTwo() {
-        var interval = window.setInterval(function () {
+        interval = window.setInterval(function () {
             $('html, body').animate({
                 scrollTop: parseInt($("#code_intro").offset().top)
             }, 500);
@@ -86,27 +101,21 @@ window.onload = function () {
             afterComplete: (instance) => {
                 clearInterval(interval);
 
-                $("#code_intro_section").animate({color:"black"});
-                $("#code_intro_section").animate({backgroundColor:"white"});
+                $("#code_intro_section").animate({color: "black"});
+                $("#code_intro_section").animate({backgroundColor: "white"});
 
 
                 $("body").append("<section id=\"education\">\n" +
-                    "    <div class=\"row\">\n" +
-                    "        <div class=\"col-md-1 mx-auto\">\n" +
-                    "            <h1><span>Education</span></h1>\n" +
-                    "        </div>\n" +
-                    "\n" +
-                    "        <div class=\"col-md-8 mx-auto\">\n" +
+                    "        <div class=\"col-10 mx-auto\">\n" +
+                    "            <h1 class=\"mb-5\"><span>Education</span></h1>\n" +
                     "            <div class=\"mb-5\"><h3>Worcester Polytechnic Institute</h3>\n" +
                     "                <p class=\"info\">B.S. Computer Science <span>&bull; </span><em class=\"date\">2020</em></p>\n" +
                     "                <p class=\"info\">Data Science Minor <span>&bull; </span>Astrophysics Minor</p>\n" +
-                    "\n" +
                     "            </div>\n" +
                     "            <div class=\"mb-5\"><h3>Bangkok Patana School</h3>\n" +
                     "                <p class=\"info\">IB Diploma <span>&bull; </span><em class=\"date\">2016</em></p>\n" +
                     "            </div>\n" +
                     "        </div>\n" +
-                    "    </div>\n" +
                     "</section>")
 
                 $('html, body').animate({
@@ -143,8 +152,8 @@ window.onload = function () {
             .break()
             .pause(300)
             .exec(async () => {
-                $("#code_intro_section").animate({color:"white"});
-                $("#code_intro_section").animate({backgroundColor:"black"});
+                $("#code_intro_section").animate({color: "white"});
+                $("#code_intro_section").animate({backgroundColor: "black"});
             })
             .type("* ")
             .break()
@@ -156,7 +165,7 @@ window.onload = function () {
             .break()
             .type("* ")
             .break()
-            .type("* I neither identify as a front-end nor a back-end developer. I am currently one of those indecisive individuals who seek the full stack-title, or a similar broad label.")
+            .type("* I neither identify as a front-end nor a back-end developer. I am currently one of those indecisive individuals who seek the full-stack title, or a similar broad label.")
             .break()
             .type("* ")
             .break()
@@ -166,8 +175,8 @@ window.onload = function () {
         code_intro.go();
     }
 
-    function stepThree(){
-        var interval = window.setInterval(function () {
+    function stepThree() {
+        interval = window.setInterval(function () {
             $('html, body').animate({
                 scrollTop: parseInt($("#code_courses").offset().top)
             }, 500);
@@ -198,11 +207,19 @@ window.onload = function () {
                 $('html, body').animate({
                     scrollTop: parseInt($("#code_skills").offset().top) - 100
                 }, 2000);
+
+                interval = window.setInterval(function () {
+                    $('html, body').animate({
+                        scrollTop: parseInt($("#code_skills").offset().top)
+                    }, 500);
+                }, 500);
+
                 new TypeIt('#code_skills', {
                     speed: 40,
                     lifeLike: false,
                     afterComplete: (instance) => {
-                        // TODO
+                        clearInterval(interval);
+
                         $("body").append("<!-- CODE -->\n" +
                             "<section class=\"mx-5 code-format\">\n" +
                             "    <div class=\"container\">\n" +
@@ -219,19 +236,17 @@ window.onload = function () {
 
                         $('html, body').animate({
                             scrollTop: parseInt($("#finishing").offset().top)
-                        }, 2000);
+                        }, 1000);
+
 
                         new TypeIt('#finishing', {
                             speed: 60
                         }).type("* Alright now for some finishing touches! Thanks for visiting!")
                             .exec(async () => {
                                 $("body").append("<section id=\"experience\">\n" +
-                                    "    <div class=\"row\">\n" +
-                                    "        <div class=\"col-md-1 mx-auto\">\n" +
-                                    "            <h1><span>Experience</span></h1>\n" +
-                                    "        </div>\n" +
-                                    "        <div class=\"col-md-7 mx-auto\">\n" +
-                                    "            <div class=\"mb-5\"><h3>Microsoft</h3>\n" +
+                                    "    <div class=\"col-10 mx-auto\">\n" +
+                                    "        <h1 class=\"mb-5\"><span>Experience</span></h1>\n" +
+                                    "        <div class=\"mb-5\"><h3>Microsoft</h3>\n" +
                                     "                <p class=\"info\">Software Engineering Intern<span> &bull;</span> <em class=\"date\">May - August 2019</em>\n" +
                                     "                </p>\n" +
                                     "                <p>Worked in the Cyber Defense Operations Center as a member of the Digital Security and Risk\n" +
@@ -255,8 +270,7 @@ window.onload = function () {
                                     "                    Weather data processing systems</p>\n" +
                                     "            </div>\n" +
                                     "        </div>\n" +
-                                    "    </div>\n" +
-                                    "</section>")
+                                    "</section>");
 
                                 $('html, body').animate({
                                     scrollTop: parseInt($("#experience").offset().top)
@@ -292,7 +306,7 @@ window.onload = function () {
 
 
                                     $("#nav-wrap a").parent().removeClass("current");
-                                    $(this).parent().   addClass("current");
+                                    $(this).parent().addClass("current");
 
 
                                     $('html, body').stop().animate({
@@ -300,17 +314,17 @@ window.onload = function () {
                                     }, 800, 'swing');
                                 });
 
-                                $(window).on('scroll', function() {
+                                $(window).on('scroll', function () {
 
                                     var h = $('header').height();
                                     var y = $(window).scrollTop();
                                     var nav = $('#nav-wrap');
 
-                                    if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+                                    if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
                                         nav.fadeOut('fast');
                                     }
                                     else {
-                                        if (y < h*.20) {
+                                        if (y < h * .20) {
                                             nav.removeClass('opaque').fadeIn('fast');
                                         }
                                         else {
@@ -339,9 +353,7 @@ window.onload = function () {
                         $("#extra_stuff").empty();
                     })
                     .break()
-                    .type("* ")
-                    .break()
-                    .type("* Feel free to check it out later! The link is https://kitzeller.github.io/webgl-portfolio/")
+                    .type("* Feel free to check it out later! But just so you know it's under some major development right now. The link is https://kitzeller.github.io/webgl-portfolio/")
                     .break()
                     .type("* ")
                     .break()
@@ -349,27 +361,32 @@ window.onload = function () {
                     .break()
                     .type("* ")
                     .break()
-                    .type("* But to be more specific let's compare them all")
+                    .type("* But to be more specific let's compare them all. The left is no experience, and the right is a lot of experience.")
                     .break()
                     .exec(async () => {
 
+                        // Skills bars
+
                         let skills = ["Javascript", "HTML", "CSS", "Java", "Ruby", "Testing"];
-                        let colors = ["green", "red", "blue", "yellow", "purple", "magenta"];
-                        let values = [350, 300, 250, 350, 0, 300];
-                        for (let skill in skills){
-                            $("#extra_stuff").append("<div style=\"border:1px solid " +  colors[skill] +";margin:10px;width:400px;text-align:center\">\n" +
-                                "  <div id=\""+ skills[skill] + "js" + "\" style=\"background:"+ colors[skill] +";height:50px;width:1px;border:1px solid "+ colors[skill] +";\">"+ skills[skill]+ "</div>\n" +
+                        let colors = ["green", "red", "cyan", "orange", "purple", "magenta"];
+                        let values = [520, 480, 350, 520, 0, 450];
+                        for (let skill in skills) {
+                            $("#extra_stuff").append("<div style=\"  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border:1px solid black;margin:10px;width:600px;font-size: 24px; text-align:center\">\n" +
+                                "  <div id=\"" + skills[skill] + "js" + "\" style=\"background:linear-gradient(to right, dark" + colors[skill] + "," + colors[skill] + ");height:50px;width:1px;\">" + skills[skill] + "</div>\n" +
                                 "</div>");
 
-                            $("#"+skills[skill]+"js").animate({
+                            $("#" + skills[skill] + "js").animate({
                                 width: values[skill] + "px"
                             }, {
-                                duration: 1000,
+                                duration: 1500,
                                 easing: "linear"
                             });
                         }
+
                     })
-                    .type("* ").go();
+                    .type("* ")
+                    .pause(500)
+                    .go();
             }
         }).pause(100)
             .type("* I also love to learn.")
@@ -406,11 +423,10 @@ window.onload = function () {
 
     startTypeIt.go();
 
-
 };
 
 /**
- * BABYLON STEPS
+ * Babylon Steps
  */
 
 var canvas, engine, scene, camera;
@@ -420,19 +436,12 @@ function initBabylon() {
     canvas = document.getElementById("renderCanvas"); // Get the canvas element
     engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-    // Create the scene space
     scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3.Black();
-
-    // Add a camera to the scene and attach it to the canvas
     camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 3, 5 * Math.PI / 12, 50, new BABYLON.Vector3(0, 5, 0), scene);
-
-    // Add lights to the scene
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
     var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
-
-    // Watch for browser/canvas resize events
     window.addEventListener("resize", function () {
         engine.resize();
     });
@@ -453,8 +462,6 @@ function addGround() {
     var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "./files/heightMap.png", 100, 100, 100, 0, 10, scene, false);
     ground.material = groundMaterial;
 
-
-    // Register a render loop to repeatedly render the scene
     engine.runRenderLoop(function () {
         camera.alpha += 0.003;
 
