77849ba4fb5317a6c3baa39b0ea9ee07de13e315
AnaghaLate <anagha20@hotmail.com>
2019-08-28 13:10:30 -0400

    basic css update (not fully)

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-let file
+let file;
 fs.readFile( './index.html', function( err, content ) {
-  file = content
-})
+    file = content;
+});
+
+let cssFile;
+fs.readFile( './style.css', function( err, content ) {
+    cssFile = content;
+});
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      response.end( file, 'utf-8' )
+      sendFile( response, 'index.html' )
       break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    case '/style.css':
+      sendFile( response, 'style.css' )
+      break;
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -19,3 +30,9 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
