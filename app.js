e91c5c4851942c63b67c0d7df07c6e375187947e
Glitch (paristhecity-a1-gettingstarted) <none>
2019-08-28 16:30:06 +0000

    🌉👜 Checkpoint ./app.js:6266626/425 ./styles.css:6266626/20

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ $(document).ready(function() {
     $(this).css("background-color", "#2B82AB");
   });
   
-  $(".option").click(function() {
+  $(".option.home").click(function() {
       $('html,body').animate({
         scrollTop: $(".title.home").offset().top},
-        'slow');
+        'ease');
   });
   
-
+  $(".option.about").click(function() {
+      $('html,body').animate({
+        scrollTop: $(".title.about").offset().top},
+        'ease');
+  });
   
-                    
+  $(".option.experience").click(function() {
+      $('html,body').animate({
+        scrollTop: $(".title.experience").offset().top},
+        'ease');
+  });
   
 });
 
