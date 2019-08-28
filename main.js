c28d304fd7259c2fe43f5f1540f255863c57b954
Peter Christakos <newtownfam@gmail.com>
2019-08-28 19:48:23 -0400

    overhauled design using bulma

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-var seeMoreButton = document.getElementById("seeMore")
-var languages = document.getElementById("languages")
-seeMoreButton.addEventListener("click", showAllLanguages)
-
-// list to hold more languages
-var moreLanguages = ["C", "C++", "Linux", "Intelx86 Assembly Language", "SQL"]
+var image = document.getElementById("github")
+var githublink = document.getElementById("githublink")
+image.addEventListener("click", openlink)
+githublink.addEventListener("click", openlink)
 
 
-function showAllLanguages() {
-   console.log("showing all programming languages...")
-   removeButton()
-   for (lang in moreLanguages) {
-    var node = document.createElement("li")
-    var text = document.createTextNode(moreLanguages[lang])
-    node.appendChild(text)
-    languages.appendChild(node)
-   }
-}
-
-function removeButton() {
-    console.log("removing see more button")
-    seeMoreButton.remove(seeMoreButton)
+function openlink() {
+    var url = new URL("https://www.github.com/newtownfam")
+    window.open(url)
 }
