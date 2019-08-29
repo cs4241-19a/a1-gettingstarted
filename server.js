8706152828a96761090b6691a2882243641ab098
TweedChristian <christiantweed@gmail.com>
2019-08-28 21:18:24 -0400

    Updated server to support multiple file types

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
 const http = require('http'),
       fs   = require('fs'),
-      port = 3000
+      mime = require('mime'),
+      port = 3000;
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+  const filename = request.url.slice(1);
+  switch(filename) {
+    case '':
+      if (request.url === '/'){
+         sendFile(response, 'index.html');
+      }
+      break;
+    default: 
+      sendFile(response, filename);
+      break;
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen( process.env.PORT || port );
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
-}
+ 
+const mimeForExt = function(ext){
+  let mime = null;
+  switch( ext ){
+    case 'png': case 'gif':
+      mime = 'image/' + ext;
+      break;
+    case 'jpeg': case 'jpg':
+      mime = 'image/jpeg';
+      break;
+    case 'htm': case 'html':
+      mime = 'text/html';
+      break;
+    case 'css':
+      mime = 'text/css';
+      break;
+    case 'js':
+      mime = 'text/javascript';
+      break;
+    default:
+      mime = 'text/plain';
+      break;
+  }
+  return mime;
+}; 
+
+const sendFile = function(response, filename){
+  const type = mime.getType(filename);
+  fs.readFile(filename, function(err, content){
+    if(err === null){
+      response.writeHeader(200, {'Content-Type': type});
+      response.end(content);
+    }
+    else{
+      response.writeHeader(404);
+      response.end('404 Error: File Not Found');
+    }
+  });
+};
