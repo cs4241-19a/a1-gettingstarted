b4e256d0ecf5508e74400a7a86a209bad8ac9f6f
John Zarate <git@jcharante.com>
2019-08-28 21:29:41 -0400

    add animation and divs

---
+++ animations.js
@@ -0,0 +1,22 @@
+var i = 0;
+var j = 0;
+var txt1 = `Hello.`;
+var txt2 = `I'm Jean Claude.`;
+var speed = 80;
+
+function typeWriter() {
+    if (i < txt1.length) {
+        document.getElementById("typewriter-1").innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else if (j < txt2.length) {
+        document.getElementById("typewriter-2").innerHTML += txt2.charAt(j);
+        j++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+
+window.onload = function() {
+    typeWriter();
+}
