33c25685052537ac160b4ad2df9ce93d60120f83
demooof <835343134@qq.com>
2019-08-28 20:07:06 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+var http = require('http')
+    , fs   = require('fs')
+    , url  = require('url')
+    , port = 8080;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+var server = http.createServer (function (req, res) {
+    var uri = url.parse(req.url)
+
+    switch( uri.pathname ) {
+        case '/':
+            sendFile(res, 'index.html')
+            break
+        case '/index.html':
+            sendFile(res, 'index.html')
+            break
+        case '/wpi.png':
+            sendFile(res, 'wpi.png', 'image/png')
+            break
+
+        default:
+            res.end('404 not found')
+    }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+// subroutines
+
+function sendFile(res, filename, contentType) {
+    contentType = contentType || 'text/html'
+
+    fs.readFile(filename, function(error, content) {
+
+
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(content, 'utf-8')
+    })
+
 }
