f82656cbbb86e9cb2c1ee230a6af2f6f9b019d1e
Glitch (a1-tvpatterson) <none>
2019-08-29 04:09:09 +0000

    🚧🎮 Checkpoint ./basic.css:6395993/96 ./index.html:6395993/16782 ./server.js:6395993/10 ./basic.js:6395993/212

---
+++ basic.js
@@ -0,0 +1,8 @@
+function viewSection(section) {
+  var i;
+  var x = document.getElementByClassName("section");
+  for(i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.
+}
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
-    case '/basic.css':
-      sendFile( response, 'basic.css' )
+    case '/basic.js':
+      sendFile( response, 'basic.js' )
       break
     default:
       response.end( '404 Error: File Not Found' )
