360b67eb1b71380dc8fd73592284e3a2db5f2221
manasmehta18 <manasmehta.18@gmail.com>
2019-08-28 22:17:08 -0400

    fixed typewriter for edu and finalized edu and about me. Also fixed javascript error and fixed navbar

--- Scripts/NavBar.js
+++ Scripts/NavBar.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     // Transition effect for navbar
     $(window).scroll(function() {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if($(this).scrollTop() > 500) {
+        // checks if window is scrolled more than 950px, adds/removes solid class
+        if($(this).scrollTop() > 950) {
             $('#navB').addClass('solid');
+            $('.nav-link').addClass('navSc');
         } else {
             $('#navB').removeClass('solid');
+            $('.nav-link').removeClass('navSc');
         }
     });
 });
