2a1d7b55da2cbecacc8475ec79e82bb08107edf1
Nicole Conill <nconill@wpi.edu>
2020-09-03 12:22:16 -0400

    Updated server to utilize CSS file and images.

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/index.css':
+      sendFile( response, 'index.css' )
+      break
+    case '/MePic1.png':
+      sendFile( response, 'MePic1.png' )
+      break
+    case '/MePic2.png':
+      sendFile( response, 'MePic2.png' )
+      break
+    case '/MePic3.png':
+      sendFile( response, 'MePic3.png' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
