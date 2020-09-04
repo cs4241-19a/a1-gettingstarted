4adc63dfeea043cd3c59c69bd3808f731043f4e9
Kirsten Hart <kirstenhart@Kirstens-MacBook-Pro.local>
2020-09-04 15:24:25 -0400

    updated HTML, CSS, and server

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const server = http.createServer( function( request,response ) {
       sendFile( response, 'index.html' )
       break
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end( index, 'utf-8')
+      //response.end( '404 Error: File Not Found' )
   }
 })
 
