721bfdf0ce8de238882185c575a62743f161e4d8
Alex Hunt <alexmhunt3@gmail.com>
2020-09-05 16:57:19 -0400

    Edited index.html
    Added information about me, with an image and some CSS to make it all look a little nice. Edited server.js to fetch image and the style guide.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http'),
       fs   = require('fs'),
       port = 3000
+const express = require('express')
+const app = express();
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
@@ -10,6 +12,12 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/style.css':
+      sendFile(response, 'style.css')
+      break;
+    case '/img/me.jpg':
+      sendFile(response, 'img/me.jpg')
+      break;
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -23,3 +31,5 @@ const sendFile = function( response, filename ) {
      response.end( content, 'utf-8' )
    })
 }
+
+app.use(express.static(__dirname + '/img/'));
