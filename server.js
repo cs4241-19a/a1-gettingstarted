f3adfea0c95bbef087910e78c77d31519aa4b1a4
Lindberg Simpson <lksimpson@wpi.edu>
2020-09-07 08:44:40 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/style.css':
+        sendFile( response, 'style.css' )
+        break
+    case '/gompei.jpeg':
+        sendFile( response, 'gompei.jpg' )
+        break
+
     default:
       response.end( '404 Error: File Not Found' )
   }
